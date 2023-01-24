ddb794150ba9029aeecbcd89f3bf71cf79071153
Washinfron Fernandes <wmfmg@hotmail.com>
2023-01-23 22:10:06 -0300

    Projeto Finalizado

---
+++ Carros1/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization.Formatters;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Carros1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] carros = new string[6];
+            
+            for(int contadorCarros = 0; contadorCarros < carros.Length; contadorCarros++) 
+            {
+                Console.WriteLine("Digite o fabricante do veículo: ");
+                carros[contadorCarros] = Console.ReadLine();
+                    
+                Console.Clear();
+            }
+            for (int contadorCarros = 0; contadorCarros < carros.Length; contadorCarros++)
+            {
+                Console.WriteLine(carros[contadorCarros]);
+                
+            }
+                Console.ReadKey();
+
+        }
+    }
+}
